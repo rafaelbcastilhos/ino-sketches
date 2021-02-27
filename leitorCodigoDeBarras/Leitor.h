86eae65ff8669030b3b158cc0ea1cdd2c837a0d4
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2021-02-27 17:36:44 -0300

    add leitor lib

---
+++ leitorCodigoDeBarras/Leitor.h
@@ -0,0 +1,38 @@
+class KbdRptParser : public KeyboardReportParser{
+    void PrintKey(byte mod, byte key);
+
+	private:
+		void OnKeyDown  (byte mod, byte key);
+		void OnKeyUp  (byte mod, byte key);
+		void OnKeyPressed(byte key);
+};
+
+void KbdRptParser::PrintKey(byte m, byte key){
+	MODIFIERKEYS mod;
+	*((byte*)&mod) = m;
+}
+
+void KbdRptParser::OnKeyDown(byte mod, byte key){
+	PrintKey(mod, key);
+	byte c = OemToAscii(mod, key);
+
+	if (c)
+		OnKeyPressed(c);
+}
+
+void KbdRptParser::OnKeyUp(byte mod, byte key){
+	PrintKey(mod, key);
+}
+
+void KbdRptParser::OnKeyPressed(byte key){
+	if(key == 19){
+		Serial.println("Leitura finalizada");
+		leituraRealizada = true;
+	}
+	else
+		codigoLido += (char)key;  
+}
+
+USB Usb;
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD> HidKeyboard(&Usb);
+KbdRptParser Prs;
